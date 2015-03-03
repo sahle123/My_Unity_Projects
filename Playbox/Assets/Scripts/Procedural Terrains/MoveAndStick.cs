121ec3f6b27491a706a16ed397b7d11faa90ddf1
Sal Alturaigi <saal4106@gmail.com>
2015-03-03 03:43:04 +0300

    Finished Procedural Terrain modification tutorial.

---
+++ Playbox/Assets/Scripts/Procedural Terrains/MoveAndStick.cs
@@ -0,0 +1,27 @@
+﻿//
+// 	Script to keep object stuck to terrain.
+//	
+//	03/03/15
+//	Nomad
+
+using UnityEngine;
+
+public class MoveAndStick : MonoBehaviour 
+{	
+	public Terrain leTerrain;
+	public float magnitude = 5f;
+	
+	void Update () 
+	{
+		Vector3 lePosition = transform.position;
+		
+		// Move according to user input
+		lePosition += new Vector3 (Input.GetAxis ("Horizontal") * magnitude, 0f, Input.GetAxis ("Vertical") * magnitude);
+		
+		// Set out height to the terrain
+		lePosition.y = leTerrain.SampleHeight (transform.position) + magnitude;
+		
+		// Update the position of the object
+		transform.position = lePosition;
+	}
+}
---
+++ Playbox/Assets/Scripts/Procedural Terrains/ProcTerrains.cs
@@ -0,0 +1,62 @@
+﻿//
+// Adds noise to the terrain to procedurally modify it. Courtesy of Julien Lynge
+// **Learn details of this code
+//	
+//	03/03/15
+//	Nomad
+using UnityEngine;
+using LibNoise;
+using System.Collections;
+
+[RequireComponent(typeof(Terrain))]
+[RequireComponent(typeof(TerrainCollider))]
+public class ProcTerrains : MonoBehaviour 
+{
+	private Terrain leTerrain;
+	private TerrainData leData;
+
+	private Perlin perlinNoise;
+
+	void Start()
+	{
+		// Get terrain data
+		leTerrain = GetComponent<Terrain> ();
+		leData = leTerrain.terrainData;
+
+		// Make some noise
+		perlinNoise = new Perlin ();
+		perlinNoise.Seed = 1002;
+
+		StartCoroutine (UpdateTerrain ());
+	}
+
+	// Continuously create new images from noise
+	private IEnumerator UpdateTerrain()
+	{
+		// Get all height information for this terrain
+		float[,] theHeights = leData.GetHeights (0, 0, leData.heightmapWidth, leData.heightmapHeight); //<-- 2-D array
+		float leWidth = leData.heightmapWidth;
+		float leHeight = leData.heightmapHeight;
+		// **refactor above code to make it more efficient
+
+		Debug.Log ("Width and height of map are: " + leWidth + ", " + leHeight + ", conversion multiplier from the heightmap" +
+						"to world coordinates is " + leData.heightmapScale + ", size of terrain overall is " + leData.size);
+
+		while (true) 
+		{
+			float depth = Time.time / 4f;
+
+			for(int i = 0; i < leWidth; ++i)
+			{
+				for(int j = 0; j < leHeight; ++j)
+				{
+					theHeights[i,j] = (float)perlinNoise.GetValue ((float)i / leWidth, depth, (float)j / leHeight);
+				}
+			}
+
+			leData.SetHeights (0, 0, theHeights);
+
+			yield return null;
+		}
+	}
+}
